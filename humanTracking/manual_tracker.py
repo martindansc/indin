65eacd49a7b8c8a892a6b1649465260d51a73ae8
urrutiaitor <aitorurrutiazubikarai@gmail.com>
2019-05-12 11:33:20 +0200

    frame100

--- humanTracking/manual_tracker.py
+++ humanTracking/manual_tracker.py
@@ -96,7 +96,7 @@ while success:
         for new_key, new_value in new_tracked.items():
             if key == new_key:
                 diff = abs(value[2] - new_value[2])
-                if 0.95 > new_value[2]/value[2]:
+                if 0.9 > new_value[2]/value[2]:
                     print("Found difference in " + str(key))
                     new_tracked[new_key][1] = new_value[1] + diff
                     new_tracked[new_key][2] = value[2]
